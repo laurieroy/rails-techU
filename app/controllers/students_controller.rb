6b24da377b5cb34549c2c04e3b1842c7d9146d25
lauretteroy <laurieroy.dev@gmail.com>
2021-01-21 11:47:38 -0700

    Add application_helper for loggedin, restricted access

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,8 @@
 class StudentsController < ApplicationController
+  skip_before_action :require_user, only: [:new, :create]
   before_action :set_student, only: [:show, :edit, :update]
+  before_action :require_same_student, only [:edit, :update]
+
   def index
     @students = Student.all
   end
@@ -39,5 +42,12 @@ class StudentsController < ApplicationController
 
   def set_student
     @student = Student.find(params[:id])
+  end
+
+  def require_same_student
+    if current_user != @student
+      flash.now[:notice] = "You can only edit your own profile"
+      redirect_to student_path(current_user)
+    end
   end
 end
