2af0a15fc8cb9d6f023d4cd3913f96029c41369c
lauretteroy <laurieroy.dev@gmail.com>
2021-01-21 10:17:13 -0700

    Add password functionality

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -34,7 +34,7 @@ class StudentsController < ApplicationController
   end
   private
   def student_params
-    params.require(:student).permit(:first_name, :last_name, :email)
+    params.require(:student).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
   def set_student
